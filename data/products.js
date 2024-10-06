8110ca4db0e5add3cee04d352556fc7aa5773b38
javascriptProject <bashawdejenu1221@gmail.com>
2024-10-06 21:33:54 +0300

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -88,7 +88,29 @@ object3.method();
 */
 
 
- 
+export let products = [];
+
+export function loadProducts(fun) {
+ const xhr = new XMLHttpRequest();
+
+ xhr.addEventListener('load', () => {
+  products =JSON.parse(xhr.response).map((productDetails) => {
+    if (productDetails.type ==='clothing') {
+  return  new Clothing(productDetails);
+    }
+  return new Product(productDetails);
+  });
+  console.log('load products');
+
+  fun();
+ });
+ xhr.open('GET', 'https://supersimplebackend.dev/products');
+ xhr.send();
+}
+
+
+
+ /*
  export const products = [
   
   {
@@ -755,4 +777,4 @@ return  new Clothing(productDetails);
   }
 return new Product(productDetails);
 });
-
+*/
